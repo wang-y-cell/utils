46c156b9fe6b7afbd8f74e09b844f573ee08a15a
wang_y <3084003139@qq.com>
2026-08-08 23:33:16 +0800

    first commit

---
+++ build/CMakeFiles/utils.dir/DependInfo.cmake
@@ -0,0 +1,24 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "F:/wy/code/vscode/c++/utils/main.cpp" "CMakeFiles/utils.dir/main.cpp.obj" "gcc" "CMakeFiles/utils.dir/main.cpp.obj.d"
+  "F:/wy/code/vscode/c++/utils/thread_pool/src/thread_pool.cpp" "CMakeFiles/utils.dir/thread_pool/src/thread_pool.cpp.obj" "gcc" "CMakeFiles/utils.dir/thread_pool/src/thread_pool.cpp.obj.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ main.cpp
@@ -0,0 +1,6 @@
+#include "thread_pool/include/thread_pool.h"
+
+int main() {
+    thread_pool th(10);
+    return 0;
+}
